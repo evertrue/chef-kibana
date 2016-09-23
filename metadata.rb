5c7f8a0de5f31a7d2d48226948c6a5a36e13f794
Eric Herot <eric.github@herot.com>
2016-09-23 17:04:07 -0400

    Pin a minimum version of libarchive

--- metadata.rb
+++ metadata.rb
@@ -7,9 +7,11 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 version '1.0.1'
 
-%w(git chef_nginx apache2 ark libarchive java runit compat_resource).each do |cb|
+%w(git chef_nginx apache2 ark java runit compat_resource).each do |cb|
   depends cb
 end
+
+depends 'libarchive', '>= 0.4.0'
 
 supports 'ubuntu'
 supports 'debian'
